7a2709875fee2604dd7cc1352aee1b4279bf7252
Allan-934 <allan.decugis@edouardgand.fr>
2026-09-18 13:53:53 +0200

    projet finit

---
+++ EmpSce2/controleurs/C_consulterEmployes.php
@@ -0,0 +1,31 @@
+<?php
+require_once "C_menu.php";
+require_once "modeles/M_service.php";
+require_once "modeles/M_employe.php";
+class C_consulterEmployes
+{
+    private $data;
+    private $controleurMenu;
+    private $modeleService;
+    private $modeleEmploye;
+    public function __construct()
+    {
+        $this->data = array();
+        $this->controleurMenu = new C_menu();
+        $this->modeleService = new M_service();
+        $this->modeleEmploye = new M_employe();
+    }
+
+    public function action_listeEmployes($codeService)
+    {
+        $this->controleurMenu->FillData($this->data);
+        if ($codeService == "all") {
+            $this->data['leService'] = null;
+        } else {
+            $this->data['leService'] = $this->modeleService->GetService($codeService);
+        }
+        $this->data['lesEmployes'] = $this->modeleEmploye->GetListe($codeService);
+        require_once "vues/v_listeEmployes.php";
+    }
+}
+?>
